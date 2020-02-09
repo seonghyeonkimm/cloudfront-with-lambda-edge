a241ac5e231b69a6de594c22d321d985b54259fb
seonghyeon <impressor7@gmail.com>
2020-02-09 18:15:50 +0900

    add default serverless application

---
+++ lambdas/index.js
@@ -0,0 +1,8 @@
+exports.handler = (event, context, callback) => {
+  const response = event.Records[0].cf.response;
+  const headers = response.headers;
+
+  headers['x-serverless-time'] = [{ key: 'x-serverless-time', value: Date.now().toString() }];
+
+  return callback(null, response);
+};
