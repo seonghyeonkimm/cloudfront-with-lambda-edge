2a1059f24873f2031eb6b17364c58eead01c30f5
seonghyeon <impressor7@gmail.com>
2020-02-09 21:18:09 +0900

    add react-helmet to react application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,58 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import {Helmet} from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
 const IndexPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Index Page
-        </a>
-      </header>
-    </div>
+    <>
+      <Helmet>
+        <title>index page</title>
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Index Page
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
 const HelloPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Page
-        </a>
-      </header>
+    <>
+      <Helmet>
+        <title>Hello page</title>
+      </Helmet>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hello Page
+          </a>
+        </header>
     </div>
+    </>
   );
 }
 
